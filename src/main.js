6609c23dda45b192248236a1ca85e3b7fdb3bad6
Adrien <39885692+1-irdA@users.noreply.github.com>
2022-10-18 23:57:42 +0200

    Update url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const app = new App({
 	target: document.body,
 	props: {
 		name: 'Solveur du compte est bon',
-		url: 'https://cebapi.alwaysdata.net/api/v1/ceb'
+		url: 'https://cebapi.alwaysdata.net/api/compute'
 	}
 });
 
